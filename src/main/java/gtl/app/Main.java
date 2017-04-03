c93df2bf6814ac9fb91152a8abd7013d96eaf45b
ZhenwenHe <zwhe@cug.edu.cn>
2017-04-03 23:25:47 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: ZhenwenHe <zwhe@cug.edu.cn>

---
+++ src/main/java/gtl/app/Main.java
@@ -0,0 +1,18 @@
+package gtl.app;/**
+ * Created by ZhenwenHe on 2017/4/3.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}

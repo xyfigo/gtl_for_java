167724d3b07f6bdd0a5542974a7a153ba26c5797
ZhenwenHe <zwhe@cug.edu.cn>
2017-04-03 23:44:10 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: ZhenwenHe <zwhe@cug.edu.cn>

--- src/main/java/gtl/app/Main.java
+++ src/main/java/gtl/app/Main.java
@@ -3,6 +3,9 @@ package gtl.app;/**
  */
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +16,22 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        StackPane rootPane = new StackPane();
+        Scene s = new Scene(rootPane, 400, 300);
 
+        Button btn = new Button("Test");
+        btn.setOnAction((e) ->
+                {
+                    System.out.println("Test");
+                }
+        );
+
+        rootPane.getChildren().add(btn);
+
+
+        primaryStage.setTitle("Test");
+        primaryStage.setScene(s);
+
+        primaryStage.show();
     }
 }
